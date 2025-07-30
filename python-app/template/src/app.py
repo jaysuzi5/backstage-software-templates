4fb13ac9341637005a3bdbd7068873c0ea293d6a
Jay Curtis <jaysuzi5@gmail.com>
2025-07-30 12:01:59 -0400

    updating OTEL settings

--- python-app/template/src/app.py
+++ python-app/template/src/app.py
@@ -4,10 +4,12 @@ import logging
 import socket
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/api/${{values.app_name}}/v1/info')
 def info():
-    logging.info('info called')
+    logger.info('info called')
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S %p on %Y-%m-%d"),
         'hostname': socket.gethostname(),
@@ -19,7 +21,7 @@ def info():
 
 @app.route('/api/${{values.app_name}}/v1/health')
 def health():
-    logging.info('health called')
+    logger.info('health called')
     return jsonify({'status': 'UP'}), 200
 
 if __name__ == '__main__':
