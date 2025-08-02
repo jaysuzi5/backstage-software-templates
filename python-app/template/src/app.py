edde4b3288d7c7e1c0e8e4b68855d620246acf8f
Jay Curtis <jaysuzi5@gmail.com>
2025-08-02 11:51:23 -0400

    Adding support for database and api calls along with OTEL for them.

--- python-app/template/src/app.py
+++ python-app/template/src/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, jsonify, request, g
 import datetime
-import time
 import logging
+import os
+import requests
 import socket
+import time
 import uuid
+from flask import Flask, jsonify, request, g
+from flask_sqlalchemy import SQLAlchemy
 from opentelemetry.sdk._logs import LoggingHandler
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
@@ -18,8 +21,21 @@ logger.addHandler(LoggingHandler())
 # Setup Flask app
 app = Flask(__name__)
 
+# Setup Database
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+db = SQLAlchemy(app)
+
 # Instrument Flask app with OTEL Trace and Metrics
 FlaskInstrumentor().instrument_app(app)
+
 
 # Middleware
 @app.before_request
@@ -52,7 +68,24 @@ def finish_response(response):
     return response
 
 
+# Define model used with sample()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+
 # Define endpoints
+@app.route('/api/${{values.app_name}}/v1/sample')
+def sample():
+    # External API call (instrumented by opentelemetry-instrumentation-requests)
+    response = requests.get('https://api.chucknorris.io/jokes/random')
+
+    # Database query (instrumented by opentelemetry-instrumentation-sqlalchemy)
+    users = db.session.execute(db.select(User)).scalars().all()
+    
+    return {"api_data": response.text, "users": [user.name for user in users]}
+
+
 @app.route('/api/${{values.app_name}}/v1/info')
 def info():
     return jsonify({
