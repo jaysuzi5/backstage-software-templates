c7d39b43ff298e1b8cabc99e5827629121e34f29
Jay Curtis <jaysuzi5@gmail.com>
2025-08-03 15:22:33 -0400

    reverting changes and going back to auto instrumentation with fastapi instrumentation

--- python/fastAPI/template/src/app.py
+++ python/fastAPI/template/src/app.py
@@ -1,59 +1,21 @@
+from fastapi import FastAPI, Depends
 import datetime
 import requests
 import socket
-from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.sdk._logs import LoggerProvider
-from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
-from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
-from opentelemetry import trace, metrics, _logs
-
 
 from db import SessionLocal, engine
 from models import Base, WeatherCurrent
 from middleware import LoggingMiddleware
 
-# Traces
-trace.set_tracer_provider(
-    TracerProvider(
-        resource=Resource.create({SERVICE_NAME: "fastapi-test"})
-    )
-)
-span_processor = BatchSpanProcessor(OTLPSpanExporter())
-trace.get_tracer_provider().add_span_processor(span_processor)
 
-# Logs
-_log_exporter = OTLPLogExporter()
-log_provider = LoggerProvider(resource=Resource.create({SERVICE_NAME: "fastapi-test"}))
-log_provider.add_log_record_processor(BatchLogRecordProcessor(_log_exporter))
-_logs.set_logger_provider(log_provider)
-
-# Metrics
-metric_reader = PeriodicExportingMetricReader(OTLPMetricExporter())
-metrics.set_meter_provider(
-    MeterProvider(resource=Resource.create({SERVICE_NAME: "fastapi-test"}), metric_readers=[metric_reader])
-)
-
-# FastAPI app
 app = FastAPI()
-
-# Instrument FastAPI
+app.add_middleware(LoggingMiddleware)
 FastAPIInstrumentor.instrument_app(app)
 
-app.add_middleware(LoggingMiddleware)
-
-
-# Initialize DB models and dependency to get DB session
+# Initialize DB models as dependency to get DB session
 Base.metadata.create_all(bind=engine)
-
 def get_db():
     db = SessionLocal()
     try:
@@ -61,9 +23,6 @@ def get_db():
     finally:
         db.close()
 
-
-
-# Define endpoints
 @app.get("/api/${{values.app_name}}/v1/sample")
 def sample(db: Session = Depends(get_db)):
     response = requests.get('https://api.chucknorris.io/jokes/random')
