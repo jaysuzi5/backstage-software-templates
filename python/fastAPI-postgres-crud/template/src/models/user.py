cacda97401b3b50d14d9e0e53e69fff7d2d5ab7a
Jay Curtis <jaysuzi5@gmail.com>
2025-08-10 09:14:55 -0400

    stating the crud functionality

---
+++ python/fastAPI-postgres-crud/template/src/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, DateTime, Integer, String
+from framework.db import Base
+from datetime import datetime, UTC
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False, index=True)
+    email = Column(String(120), unique=True, nullable=False, index=True)
+    full_name = Column(String(100), nullable=True)
+    create_date = Column(DateTime, default=lambda: datetime.now(UTC))
+    update_date = Column(
+        DateTime,
+        default=lambda: datetime.now(UTC),
+        onupdate=lambda: datetime.now(UTC)  # auto-update on change
+    )
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.username}', email='{self.email}')>"
